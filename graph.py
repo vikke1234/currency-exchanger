8afb0c94f2db9c7266516febaa78eb67bbd4e74b
vikke1234 <vikke1234@users.noreply.github.com>
2016-07-02 16:21:33 +0300

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,4 +1,18 @@
 # coding=utf-8
+
+#Copyright (C) 2016 Viktor Horsmanheimo <vhh790@gmail.com>
+
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or (at 
+#your option) any later version.
+
+#This program is distributed in the hope that it will be useful, but
+#WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#General Public License for more details.
+
+
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import matplotlib.dates as mdates
